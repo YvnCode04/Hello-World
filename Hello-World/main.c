8256ba9f3e2f15a1d5f2021efcaac996f4f78ab1
Yvan Gauthier <y.gauthier04@icloud.com>
2024-08-05 20:21:39 +0200

    Initial Commit

---
+++ Hello-World/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Hello-World
+//
+//  Created by Yvan Gauthier on 05/08/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
